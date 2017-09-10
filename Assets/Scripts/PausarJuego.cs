c9b9e3fc9ceef5b0f862726f79ff6f58b3d9346a
Leandro <angleschecter@gmail.com>
2017-09-09 22:33:28 -0300

    Menu de pausa

---
+++ Assets/Scripts/PausarJuego.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausarJuego : MonoBehaviour {
+
+    public Transform canvas;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pausar();
+        }
+    }
+        public void Pausar() {
+            if (canvas.gameObject.activeInHierarchy == false) {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            } else {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+
+            }
+
+        }
+    }
