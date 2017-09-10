c7e92b536a2d9843270a3d7e4044d587f608cc3f
Leandro <angleschecter@gmail.com>
2017-09-10 17:08:02 -0300

    Menu Principal, Botón de salir de menu de pausa lleva a menu principal

---
+++ Assets/Scripts/MenuPpal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPpal : MonoBehaviour {
+
+	public void BtnNuevoJuego(string nivelNuevoJuego)
+    {
+        SceneManager.LoadScene(nivelNuevoJuego);
+    }
+
+    public void BtnSalirJuego()
+    {
+        Application.Quit();
+    }
+}
