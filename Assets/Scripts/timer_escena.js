ada32ac14a3d865b79cc21cb6c89bd1bb5faef70
iniguezlucila <iniguez.lucila@gmail.com>
2017-09-15 09:30:13 -0300

    Saltear cinematica

--- Assets/Scripts/timer_escena.js
+++ Assets/Scripts/timer_escena.js
@@ -3,11 +3,9 @@ var tiempo_end : float = 0.0;
 var escena : String; 
 function Update (){
     tiempo_start += Time.deltaTime;
-    if (tiempo_start>=tiempo_end) 
+    if (tiempo_start>=tiempo_end || Input.GetKeyDown(KeyCode.Mouse0)) 
 	{
         Application.LoadLevel(escena); 
     }
-
-
 
 }
