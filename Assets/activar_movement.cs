022d16bae78b62569ae9a722eb9cbfdf3e371283
Mariano <mariano.gobbi@hotmail.com>
2017-10-01 18:43:56 -0300

    Bosque segunda opcion avanzado hasta antes del runner.

---
+++ Assets/activar_movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activar_movement : MonoBehaviour {
+
+    private GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            Player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            Player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+
+
+        }
+    }
+}
